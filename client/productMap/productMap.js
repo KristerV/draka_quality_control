515abf1aea9fdb779a1b215f39cca8548126a0d6
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-06-22 12:54:42 +0300

    semi-fix comma

--- client/productMap/productMap.js
+++ client/productMap/productMap.js
@@ -15,11 +15,26 @@ Template.productMap.events({
 
 ProductMap = {
 	updateField: function(event, field) {
+
+		// Get values
 		var id = $(event.currentTarget).parents('[data-map-id]').attr('data-map-id')
 		var value = $(event.currentTarget).val()
 		var data = {}
+
+		// Check for commas
+		if (value.indexOf(',') >= 0) {
+			sAlert.error('Palun kasuta koma asemel punkti, nt. 0.34')
+			return false
+		} else if (!value) {
+			sAlert.error('Kindel, et kirjutasid numbri? Muide, koma asemel kasutame punkti.')
+			return false
+		}
+
+		// Save
 		data[field] = value
 		ProductMapCollection.update(id, {$set: data})
+
+		// Notify
 		sAlert.success("salvestatud", {timeout: 2000})
 	}
 }
