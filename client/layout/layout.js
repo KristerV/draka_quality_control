ad42188e2137f8f560ce2e1b81390d88ac65e635
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-08-02 11:39:04 +0300

    export filename fix

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -5,7 +5,7 @@ Template.Layout.events({
 				sAlert.error("Error: " + err)
 
 			if(fileContent){
-				var nameFile = "export-" + moment().format("d-MM-YYYY") + ".csv"
+				var nameFile = "quality-export-" + moment().format("D-MM-YYYY") + ".csv"
 				var blob = new Blob([fileContent], {type: "text/plain;charset=utf-8"});
 				saveAs(blob, nameFile);
 			}
