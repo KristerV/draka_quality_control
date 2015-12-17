5844b7b86ebedb1282467bc86eab7e58d0e4a817
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-12-17 12:57:37 +0200

    increase interval of KSM timer

--- server/timer.js
+++ server/timer.js
@@ -1,7 +1,8 @@
 
-var delay = 24 * 60 * 60 * 1000
-
 Meteor.setInterval(function(){
 	Meteor.call('importData')
+}, 24 * 60 * 60 * 1000)
+
+Meteor.setInterval(function(){
 	Meteor.call('import-KSM-data')
-},delay)
+}, 5 * 60 * 1000)
