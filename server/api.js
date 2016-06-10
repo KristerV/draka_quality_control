e26c991ece79c7bd9865ac0c56500395617ecb9e
Kaur Kirikall <kaur.kirikall@gloch.eu>
2016-06-10 11:06:57 +0300

    Service configuration lisatud

---
+++ server/api.js
@@ -0,0 +1,43 @@
+Meteor.startup(function () {
+	var Api = new Restivus({
+			useDefaultAuth: false,
+			prettyJson: true,
+			enableCors: true
+		});
+
+		Api.addCollection(ProductMapCollection);
+		Api.addCollection(ProductsCollection);
+
+		Api.addRoute('add', {authRequired: false}, {
+			post: function() {
+				var tmp = ProductsCollection.insert(this.bodyParams);
+				if(tmp){
+				  return {status: 'success', data: tmp};
+				}
+				return {
+				  statusCode: 400,
+				  body: {status: 'fail', message: 'Unable to CREATE subscriber!'}
+				};
+				/*var product = this.urlParams.string;
+				var query = this.queryParams;
+				console.log(product);
+				console.log(query);
+				return*/
+			}
+		});
+
+		Api.addRoute('search/:string', {authRequired: false}, {
+			get: function() {
+				var string = this.urlParams.string;
+				if(string.match(/^\d+$/)){
+					return ProductMapCollection.findOne({'code': parseInt(string)});
+				}
+				else{
+					return ProductMapCollection.findOne({'description': string});
+				}
+		    }
+		});
+
+		
+
+});
