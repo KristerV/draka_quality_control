a6b022c423e0acda5dc7b59284f2bdbd59d4114e
Krister Viirsaar <git@krister.ee>
2021-04-06 09:59:00 +0300

    Change comma to period in numbers

--- client/index.js
+++ client/index.js
@@ -2,3 +2,11 @@ Session.setDefault('filterOotel', true)
 Session.setDefault('filterKatsetamisele', true)
 Session.setDefault('filterKinnitatud', false)
 Session.setDefault('KSM-range', 'last20')
+
+// Convert commas to periods so it's more comfy for workers
+document.addEventListener('keydown', function(e){
+    if (e.target.tagName === "INPUT" && e.target.type === "number" && e.key === ",") {
+        e.target.value += '.'
+        e.preventDefault()
+    }
+})
