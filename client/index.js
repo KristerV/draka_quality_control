4c67c02f42072d88098178efe4e0f8ef8ca2f35a
Krister Viirsaar <git@krister.ee>
2021-11-04 13:11:59 +0000

    Stop Chrome from deleting input value

--- client/index.js
+++ client/index.js
@@ -6,7 +6,14 @@ Session.setDefault('KSM-range', 'last20')
 // Convert commas to periods so it's more comfy for workers
 document.addEventListener('keydown', function(e){
     if (e.target.tagName === "INPUT" && e.target.type === "number" && e.key === ",") {
+        var val = e.target.value
         e.target.value += '.'
         e.preventDefault()
+
+        // Chrome can't handle a rogue dot so it just deletes the value. Revert it.
+        if (e.target.value === "") {
+            e.target.value = val
+            sAlert.error("Palun kasuta koma asemel punkti. Või proovi Firefoxi.")
+        }
     }
 })
